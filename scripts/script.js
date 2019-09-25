e801da81a7b97b56ad6b595f6af302846071a0cb
Ashley Clarke <crashley92@gmail.com>
2019-09-24 19:41:01 -0700

    removed onload() and am tring document.ready

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,6 @@
 //onload the page will have the welcome and nabar fade in
+$(document).ready(function() {
   //welcome fade in
   $(".display-4").hide();
-  $(".display-4").load("index.html");
-  $(".display-4").fadeIn(3000);
-
-
+  $(".display-4").fadeIn(3000);  
+});
