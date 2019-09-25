3b6b29e53b5999cb3343df6ec5e464f50e999f0e
Ashley Clarke <crashley92@gmail.com>
2019-09-24 19:37:36 -0700

    removed document onload since .load() method is already of written jquery script.

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,7 @@
 //onload the page will have the welcome and nabar fade in
-$(document).ready(function() {
   //welcome fade in
   $(".display-4").hide();
   $(".display-4").load("index.html");
   $(".display-4").fadeIn(3000);
 
-  
-});
+
